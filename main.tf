26cfaf132f707a830d9e9e2f7c7386d9c11ea7b7
Cosmin Tudor <cosmin.tudor82@gmail.com>
2019-09-09 12:07:25 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,12 @@ terraform {
   required_version = ">= 0.12.0"
 }
 
+#provider "aws" {
+#}
+
 provider "aws" {
+  # MODIFY this line to look for 2.27.0 or greater
+  version = ">= 2.27.0"
 }
 
 module "server" {
